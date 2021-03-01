266532200008f7def9c7fee4ea6a0bbc46d1e374
Long <61956286+dogialong1212@users.noreply.github.com>
2021-03-01 09:33:51 +0700

    32

---
+++ project_code/Detect/detect.h
@@ -0,0 +1,42 @@
+#ifndef DETECT_H
+#define DETECT_H
+
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <QTime>
+#include <QThread>
+#include <QPixmap>
+#include <QDebug>
+
+
+#define ID_CAMERA 0
+
+class detect: public QThread
+{
+    Q_OBJECT
+public:
+    detect(QObject *parent = nullptr);
+    QPixmap pixmap() const
+    {
+        return mPixmap;
+    }
+    std::vector<std::vector<double>> buffer;
+signals:
+    void newPixmapCaptured();
+protected:
+    void run() override;
+private:
+    QTime m_time;
+    QPixmap mPixmap;
+    cv::Mat mFrame, blob ;
+    cv::VideoCapture mVideoCap;
+    QImage cvMatToQImage(const cv::Mat &inMat);
+    QPixmap cvMatToQPixmap(const cv::Mat &inMat);
+};
+
+#endif // DETECT_H
+
---
+++ project_code/Detect/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class test;
+class detect;
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+    void on_CameraOn_Button_clicked();
+
+    void on_CameraOff_Button_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    detect *VideoCapture;
+
+};
+#endif // MAINWINDOW_H
